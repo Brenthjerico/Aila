b8b719ab571561b9f0047b30cefcf82bd99d1b38
Brenthjerico <113017861+Brenthjerico@users.noreply.github.com>
2024-12-11 20:18:10 +0800

    last coms

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 function showLoveMessage() {
     const surprise = document.getElementById('surprise');
     surprise.style.display = 'block'; // Show the love message
+    
 }
+function showGift() {
+    const giftPopup = document.getElementById('giftPopup');
+    giftPopup.style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+}
+
+function closePopup() {
+    document.getElementById('letterPopup').style.display = 'none';
+    document.getElementById('giftPopup').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+}
+function closeGiftPopup() {
+    document.getElementById('giftPopup').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+}
+
